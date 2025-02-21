2959e984ba2403af0191a368ddfef561f74fa403
Daniel-derf <danielpardinho321@gmail.com>
2025-02-21 18:18:07 -0300

    emmiter core

--- emmiter.js
+++ emmiter.js
@@ -1,5 +1,18 @@
-function Emmiter() {
+function Emitter() {
   this.events = {};
 }
 
-Emiter.prototype.on = function (type, listener) {};
+Emiter.prototype.on = function (type, listener) {
+  this.events[type] = this.events[type] || [];
+  this.events[type].push(listener);
+};
+
+Emitter.prototype.emit = function (type) {
+  if (this.events[type]) {
+    this.events[type].forEach(function (listener) {
+      listener();
+    });
+  }
+};
+
+module.exports = Emitter;
